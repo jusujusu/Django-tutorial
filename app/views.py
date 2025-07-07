05be2b7854841899cbf887455ed40ff6164dbf43
JunSu <skfkahdk754@gmail.com>
2025-07-07 17:57:56 +0900

    added app/index view

--- app/views.py
+++ app/views.py
@@ -1,3 +1,23 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from app.models import Post
 
 # Create your views here.
+def index(request : HttpRequest) -> HttpResponse:
+
+
+    # qs = [
+    #     {"id": 1, "title": "post #1"},
+    #     {"id": 2, "title": "post #2"}
+    # ]
+
+    # db의 post로 대체
+    qs = Post.objects.all()
+
+    return render(
+        request,
+        "app/index.html",
+        {
+            "post_list" : qs
+        }
+    )
